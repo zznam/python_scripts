ab418dc3ec1c3da85cc85080e50e4e012c088b8c
zznam <zznam.hust@gmail.com>
2020-10-11 01:08:03 +0700

    python data structures for everyone coursera

---
+++ 1_helloworld3.py
@@ -0,0 +1,15 @@
+print("hello world")
+# name = input('What is your name?\n')
+name = "Anna"
+print('n is your name:', 'n' in name) # using in as a Logical Operator
+print('m is your name:', 'm' in name)
+print('index of n is your name:', name.find('n'))
+print('index of m is your name:', name.find('m'))
+
+print('apple' == 'Apple')
+print('apple' > 'Apple')
+print('A unicode is', ord('A'), ',a unicode is', ord('a'))
+
+stuff = 'Hello world'
+print(type(stuff))
+print(dir(stuff))
---
+++ 3_strings_upper_strip.py
@@ -0,0 +1,9 @@
+x = '40'
+y = int(x) + 2
+print(y)
+
+greet = 'Hello Bob'
+print(greet.upper())
+
+myString = "  zznam 34      "
+print(myString.strip())
